c4d63b5d544693da3fa4e161473f11c2d31648b8
Dennis Miceli <dennis.miceli@hotmail.ch>
2022-02-26 20:17:05 +0100

    Add Bottom sticked button and textfield

--- src/components/pages/LandingPage.tsx
+++ src/components/pages/LandingPage.tsx
@@ -1,10 +1,38 @@
+import {
+  BottomNavigation,
+  Container,
+  Fab,
+  Grid,
+  Paper,
+  TextField,
+} from "@mui/material";
 import moment from "moment";
+import AddIcon from "@mui/icons-material/Add";
 
 export default function LandingPage() {
-  console.log(moment().format("dd, hh:mm, DD.MM.YYYY"))
-
+  console.log(moment().format("dd, hh:mm, DD.MM.YYYY"));
 
   return (
-    <div>LandingPage</div>
-  )
+    <>
+      <Container maxWidth="md">
+        <Paper
+          sx={{ position: "fixed", bottom: 0, left: 0, right: 0 }}
+          elevation={3}
+        >
+          <Grid container spacing={1} alignItems="center" justifyContent="center" marginY={2}>
+            <Grid item xs={"auto"}>
+              <TextField
+                id="standard-basic"
+                label="Standard"
+                variant="standard"
+              />
+              <Fab color="primary" size="medium" aria-label="add">
+                <AddIcon />
+              </Fab>
+            </Grid>
+          </Grid>
+        </Paper>
+      </Container>
+    </>
+  );
 }
